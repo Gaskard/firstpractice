6e8ba4385396197d3a4437053f6130656632522d
Dmytro Shaposhnikov <dimacr137@gmail.com>
2024-06-09 20:04:39 +0300

    add cycles and exam

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 "use strict";
 
+
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
 
-const a = prompt("Один из последних просмотренных фильмов?", ''),
-      b = prompt("На сколько оцените его?", ''),
-      c = prompt("Один из последних просмотренных фильмов?", ''),
-      d = prompt("На сколько оцените его?", '');
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -15,8 +13,30 @@ const personalMovieDB = {
     privat: false
 };
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено мало фильмов');
+} else if (personalMovieDB.count <= 30) {
+    alert('Просмотрено среднее количество фильмов');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман!');
+} else {
+    alert('error');
+}
+
+
+
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов?", ''),
+              b = prompt("На сколько оцените его?", '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    };
+
+
 
 console.log(personalMovieDB)
 
